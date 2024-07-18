e6663f82d739e0d170f1d1f363aca28d2ff95209
José Bernardino Jr <jbernardino.jr@gmail.com>
2024-07-17 21:03:26 -0300

    [BUSLocationSP] First Commit with initial files and Google Maps API config files
    Signed-off-by: José Bernardino Jr <jbernardino.jr@gmail.com>

---
+++ app/src/main/java/br/com/bernardino/buslocationsp/MainActivity.kt
@@ -0,0 +1,72 @@
+package br.com.bernardino.buslocationsp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.bernardino.buslocationsp.ui.theme.BusLocationSPTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BusLocationSPTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    /*Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )*/
+                    Maps(name = "Singapura", innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Maps(name: String, innerPadding: Modifier) {
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BusLocationSPTheme {
+        Greeting("Android")
+    }
+}
